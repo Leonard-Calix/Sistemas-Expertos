e060fa496a6286f7bae6a07c7aff97d52e9326c6
Leonard <36576933+Leonard-Calix@users.noreply.github.com>
2020-02-11 11:55:24 -0600

    agregando booptrap

--- Proyecto/src/app/app.module.ts
+++ Proyecto/src/app/app.module.ts
@@ -3,16 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { NavbarComponent } from '@components/navbar/navbar.component';
+import { NavbarService } from '@services/navbar.service';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    NavbarService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
