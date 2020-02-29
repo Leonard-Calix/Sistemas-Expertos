c6af7cbe157ed4d353f3e0a364887e5cb7f09b69
Leonard <36576933+Leonard-Calix@users.noreply.github.com>
2020-02-29 06:21:48 -0600

    prueba

--- Proyecto/src/app/components/administracion/resumen/resumen.component.ts
+++ Proyecto/src/app/components/administracion/resumen/resumen.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-resumen',
@@ -7,15 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResumenComponent implements OnInit {
 
-  constructor() { }
+  constructor(private renderer:Renderer2, private elemento:ElementRef) { }
 
   ngOnInit(): void {
 
-    console.log(this.div);
+    let targeta = `<div class="card" style="width: 18rem;">
+                  <img class="card-img-top" src="..." alt="Card image cap">
+                  <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+                </div>`;
+
+    let div = this.renderer.createElement('div');
+    let card = this.renderer.createElement(targeta);
+
+  
+    this.renderer.appendChild(div, card);
+    this.renderer.appendChild(this.elemento.nativeElement, div);
+
+   
+
+
+
+
+    
   }
-
-  boton:string = '<a class="btn btn-sm " routerLink="">Prueba</a>';
-
-  div = document.getElementById("contenido");
 
 }
