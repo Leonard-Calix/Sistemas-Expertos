3a01d14c0dae535ea42898cb3115347708b9d008
Leonard <36576933+Leonard-Calix@users.noreply.github.com>
2020-03-01 18:38:44 -0600

    prueba

--- Proyecto/src/app/components/administracion/resumen/resumen.component.ts
+++ Proyecto/src/app/components/administracion/resumen/resumen.component.ts
@@ -11,28 +11,6 @@ export class ResumenComponent implements OnInit {
 
   ngOnInit(): void {
 
-    let targeta = `<div class="card" style="width: 18rem;">
-                  <img class="card-img-top" src="..." alt="Card image cap">
-                  <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                  </div>
-                </div>`;
-
-    let div = this.renderer.createElement('div');
-    let card = this.renderer.createElement(targeta);
-
-  
-    this.renderer.appendChild(div, card);
-    this.renderer.appendChild(this.elemento.nativeElement, div);
-
-   
-
-
-
-
-    
   }
 
 }
--- Proyecto/src/app/services/carga-imagenes.service.ts
+++ Proyecto/src/app/services/carga-imagenes.service.ts
@@ -1,9 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-
-
-
 @Injectable({
   providedIn: 'root'
 })
@@ -11,16 +8,19 @@ export class CargaImagenesService {
 
   private CARPETA_IMAGENES = 'img';
 
+  urlServicio:string = 'http://localhost:4300/';
+
   constructor(private httpCient:HttpClient) { }
 
   ngOnInit(): void {
   }
-
+ 
+  
 
   cargarImagenes(archivo){
 
     let url = 'http://localhost:4300/cargarImagen';
-    this.httpCient.post(url, archivo ).subscribe(datos => console.log(datos));
+    this.httpCient.post(this.urlServicio+'cargarImagen' , archivo ).subscribe(datos => console.log(datos));
     
   }
 
@@ -30,7 +30,5 @@ export class CargaImagenesService {
     this.httpCient.get(url).subscribe(datos => console.log(datos));
     
   }
-
-
 
 }
