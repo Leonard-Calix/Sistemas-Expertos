cd5f0bd62f560431e93464fca87d432027d34da9
Leonard <36576933+Leonard-Calix@users.noreply.github.com>
2020-04-21 22:13:27 -0600

    imagenes

---
+++ Proyecto/back-end/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var cors = require('cors');
+var bodyParser = require('body-parser');
+
+
+var app = express();
+app.use(express.static('www'));
+app.use(cors());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use( require( './routes/usuario' ) );
+app.use( require( './routes/sitio' ) );
+app.use( require( './routes/imagen' ) );
+app.use( require( './routes/blog' ) );
+app.use( require( './routes/SCSitio' ) );
+app.use( require( './routes/SCblog' ) );
+app.use( require( './routes/categoria' ) );
+app.use( require( './routes/comentario' ) );
+
+app.listen('4300',function(){
+    console.log('Servidor levantado... en el PUERTO 4300')
+});
+
